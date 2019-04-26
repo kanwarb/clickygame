bc258e2dbe515ac59a6f46e1cf405d43054338cb
KB Total Recall <kanwar@gmail.com>
2019-04-25 21:14:01 -0700

    Final update to click game

--- clickgame/src/App.js
+++ clickgame/src/App.js
@@ -8,7 +8,7 @@ function App() {
   return (
     <Router>
     <div className="App">
-      <Route exact path="/" component={game} />
+      <Route exact path="/" component={game} /> 
      </div>
      </Router>
   );
