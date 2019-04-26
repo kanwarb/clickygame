3f03706f8b9c0ad7095d9f13628fea2b20ba4a22
KB Total Recall <kanwar@gmail.com>
2019-04-25 20:40:41 -0700

    Add Score capture for player

--- clickgame/src/game.js
+++ clickgame/src/game.js
@@ -1,7 +1,8 @@
-import React, {Component } from "react";
+import React, { Component } from "react";
 import crafts from "./clickcard.json";
 import Card from "./components/Card";
-import Wrapper from "./components/Wrapper";
+import Score from "./components/Score";
+
 class game extends Component {
     state = {
         crafts,
@@ -9,47 +10,56 @@ class game extends Component {
         score: 0,
         maxscore: 8,
         status: ""
-      };
+    };
     scoreCount = (id) => {
         let selectedCraftId = this.state.selectedCraftId;
-    
-        if(selectedCraftId.includes(id)){
-          this.setState({ selectedCraftId: [], score: 0, status:  "You Lost the Game" });
-          return;
-        }else{
-          selectedCraftId.push(id)
-    
-          if(selectedCraftId.length === 9){
-            this.setState({score: 9, status: "You've Landed all the crafts", selectedCraftId: []});
-            console.log('You Win');
-            return;
-          }
-    
-          this.setState({ selectedCraftId, score: selectedCraftId.length, status: " " });
-    
-          for (let i = crafts.length - 1; i > 0; i--) {
-            let j = Math.floor(Math.random() * (i + 1));
-            [crafts[i], crafts[j]] = [crafts[j], crafts[i]];
-          }
-        }
-      }
 
-      render(){
-          return(
+        if (selectedCraftId.includes(id)) {
+            this.setState({ selectedCraftId: [], score: 0, status: "You Lost the Game" });
+            return;
+        } else {
+            selectedCraftId.push(id)
+
+            if (selectedCraftId.length === 12) {
+                this.setState({ score: 12, status: "You've Landed all the crafts", selectedCraftId: [] });
+                console.log('You Win');
+                return;
+            }
+
+            this.setState({ selectedCraftId, score: selectedCraftId.length, status: " " });
+
+            for (let i = crafts.length - 1; i > 0; i--) {
+                let j = Math.floor(Math.random() * (i + 1));
+                [crafts[i], crafts[j]] = [crafts[j], crafts[i]];
+            }
+        }
+    }
+
+    render() {
+        return (
             <div className="App-container">
-            <h1>Click on image only once to win</h1>
-            {crafts.map(craft => (
-              <Card
-                scoreCount={this.scoreCount}
-                id={craft.id}
-                key={craft.id}
-                image={craft.image}
-              />
-  
-            ))}
+                <header className="App-header">
+                    <h1 className="App-title">Clicky Game</h1>
+                    <p className="App-intro">
+                        Try to click each image once 
+          </p>
+                </header>
+                <Score total={this.state.score}
+                    goal={12}
+                    status={this.state.status}
+                />
+                {crafts.map(craft => (
+                    <Card
+                        scoreCount={this.scoreCount}
+                        id={craft.id}
+                        key={craft.id}
+                        image={craft.image}
+                    />
+
+                ))}
             </div>
-          )
-      }
+        )
+    }
 
 }
 
