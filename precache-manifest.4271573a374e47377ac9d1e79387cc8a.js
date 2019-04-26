6a1f04f7a4e4c97bb3f905ebef04f8b50276ca51
KB Total Recall <kanwar@gmail.com>
2019-04-25 20:46:23 -0700

    Updates

---
+++ precache-manifest.4271573a374e47377ac9d1e79387cc8a.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "66898032b492b125795c8fa062e66e92",
+    "url": "/clickgame/index.html"
+  },
+  {
+    "revision": "895afc9b79dc4776f588",
+    "url": "/clickgame/static/css/main.5272b749.chunk.css"
+  },
+  {
+    "revision": "ab8013d9fece1992d969",
+    "url": "/clickgame/static/js/2.bd72b94f.chunk.js"
+  },
+  {
+    "revision": "895afc9b79dc4776f588",
+    "url": "/clickgame/static/js/main.1cf1d709.chunk.js"
+  },
+  {
+    "revision": "550c79de082ae6f7d9ff",
+    "url": "/clickgame/static/js/runtime~main.5bc58040.js"
+  }
+]);
